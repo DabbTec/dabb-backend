b80ed81ead5862c5e0db66f0d2cc26869873f695
DabbTec <abbeyzino51@gmail.com>
2025-11-18 14:14:41 -1100

    new

--- api/db.js
+++ api/db.js
@@ -1,6 +1,15 @@
 import postgres from 'postgres'
 
 const connectionString = process.env.DATABASE_URL
-const sql = postgres(connectionString)
+
+// When running on serverless platforms (like Vercel) the Postgres server
+// requires TLS. Ensure we pass SSL options so connections don't fail with
+// certificate validation errors. For local development this will work
+// the same as before.
+const sql = postgres(connectionString, {
+	ssl: {
+		rejectUnauthorized: false,
+	},
+})
 
 export default sql
